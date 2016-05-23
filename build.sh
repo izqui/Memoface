0c82c56fb05d49c5135377474a3606e67e11be2a
MFava91 <mattiafa91@gmail.com>
2016-05-24 00:21:19 +0200

    Remove unnecessary files

--- build.sh
+++
@@ -1,3 +0,0 @@
-echo "COMPILING JS"
-cat js/*.js | uglifyjs -o src/js/pebble-js-app.js
-echo "DONE COMPILING"
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-cp build/Chebble.pbw ../izqui.me/files/Chebble.pbw
-cp html/chebble.html ../izqui.me/html/chebble.html
-cd ../izqui.me
-git commit -a -m "New Chebble binary"
-git push vps master
-cd ../Chebble
